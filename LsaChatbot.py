e64c8f07246dacfb7ac66f1a0579973dc40f0613
Ahmad Maruf <ahmadmaruf2701@gmail.com>
2023-07-19 19:08:06 +0700

    add comment and change result format

--- LsaChatbot.py
+++ LsaChatbot.py
@@ -7,49 +7,72 @@ from bahasa.stemmer import Stemmer
 from gensim import corpora, models, similarities
 
 class LsaChatbot:
+    # Inisiasi objek LSA
     def __init__(self, stopword_path: str, dataset_path: str):
+        # lokasi/alamat path dataset (dataset/dataset.csv)
         self.dataset_path = dataset_path
+        # lokasi alamat path stopword (stopword.txt)
         self.stopword_path = stopword_path
+
         self.stopwordMachine = StopwordRemoverMachine()
         self.slangMachine = SlangConverterMachine()
         self.stemmer = Stemmer()
         self.stopwordMachine.importDictionary(self.stopword_path)
 
-        # for data training
+        # mengambil data dataset
         self.dataset = self.load_data()
 
+        # variable yang berisi data hasil training
         self.trained = None
         self.train()
             
+    # load/mengambil dataset dari CSV
     def load_data(self):
         return pd.read_csv(self.dataset_path)
     
+    # PREPROCESSING DATASET
     def pre_process(self, text):
-        text = text.lower()
+        # CASE FOLDING - ubah text menjadi lowecase/huruf kecil
+        text = text.lower() 
+        # TOKENIZE - membuat token kata (kalimat dipecah menjadi list kata)
         tokens = tokenize(text)
+        # SLANG CONVERTER - konversi bahasa gaul menjadi bahasa baku pada token
         tokens = self.slangMachine.convertSlang(tokens)
+        # FILTERING STOPWORD - penghapusan stopword pada token
         tokens = self.stopwordMachine.removeStopword(tokens)
+        # STEMMING token kata
         tokens = [self.stemmer.stem(t) for t in tokens]
+        # output/hasil preprocess
         return (tokens)
 
     def train(self):
-        # reset
+        # reset data training sebelumnya
         data = self.load_data()
         self.trained = None
-        # preprocess
+
+        # preprocess untuk setiap baris "message" pada dataset >> LIHAT PRE_PROCESS DIATAS
         data['MESSAGE'] = data['MESSAGE'].apply(self.pre_process)
-        question = data['MESSAGE']
+        # kumpulan data pertanyaan yang telah di pre-process
+        question = data['MESSAGE'] 
 
-        # LSA
+        # ===== PERHITUNGAN LSA ========
+        # Mapping kata/token unik menjadi id angka
         dictionary = corpora.Dictionary(question)
+        # DOCUMENT-TERM MATRIX
         corpus = [dictionary.doc2bow(q) for q in question]
-        tfidf = models.TfidfModel(corpus)
 
+        # TF-IDF
+        tfidf = models.TfidfModel(corpus)
         corpus_tfidf = tfidf[corpus]
+
+        # model LSA (proses SVD ada didalam model ini)        
         lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=50)
         corpus_lsi = lsi[corpus_tfidf]
+
+        # SIMILIARITY
         index = similarities.MatrixSimilarity(corpus_lsi)
     
+        # output model training LSA
         self.trained = {
             'dictionary'    : dictionary,
             'corpus'        : corpus,
@@ -65,25 +88,32 @@ class LsaChatbot:
         
         return self.trained
     
+    # proses di chatbot ( input pertanyaan, cari data jawaban terdekat dengan model LSA yang dibuat sebelumnya)
     def talk(self, question:str):
+        # PRE-PROCESS PERTANYAAN
         tokens = self.pre_process(question)
         text = " ".join(tokens)
 
-        # define trained data
+        # DEFINE HASIL TRAINING DATA MODEL SEBELUMNYA
         dictionary = self.trained['dictionary']
         tfidf = self.trained['tfidf']
         lsi = self.trained['lsi']
         index = self.trained['index']
 
-        # find and sort similarity
-        vec_bow = dictionary.doc2bow(text.lower().split())
+        # === PROSES LSA dari input pertanyaan ====
+        # document-term matrix
+        vec_bow = dictionary.doc2bow(tokens)
+        # TF-IDF
         vec_tfidf = tfidf[vec_bow]
+        # LSA/SVD
         vec_lsi = lsi[vec_tfidf]
 
+        # jika tidak ditemukan hasilnya
         if not(vec_lsi):
             not_understood = "maaf, saya tidak paham yang anda maksud, bisa anda ulangi?"
-            return not_understood
+            payload = not_understood
         else:
+            # lihat urutan index terdekat (hasil perhitungan LSA)
             sims = index[vec_lsi]
             sims = sorted(enumerate(sims), key=lambda item: -item[1])
             index_s = []
@@ -91,7 +121,7 @@ class LsaChatbot:
             for i in range(len(sims)):
                 x = sims[i][1]
 
-                # is similarity is less than 0.5 
+                # JIKA Similiarity < 0.8 (muncul 3 opsi pertanyaan terdekat)
                 if x <= 0.8:
                     index_s.append(str(sims[i][0]))
                     score_s.append(str(sims[i][1]))
@@ -103,40 +133,45 @@ class LsaChatbot:
                     question_data = self.dataset['MESSAGE']
 
                     if (r_index < len(question_data) - 3):
-                        return {
-                            'status' : 200,
-                            'result' : {
-                                'payload' : {
-                                    'quick_replies': [
-                                        {
-                                            'content_type' : 'text',
-                                            'message' : question_data[r_index],
-                                        },{
-                                            'content_type' : 'text',
-                                            'message' : question_data[r_index+1],   
-                                        },{
-                                            'content_type' : 'text',
-                                            'message' : question_data[r_index+2],
-                                        }
-                                    ]
-                                }
+                        payload = [
+                            {
+                                'message' : question_data[r_index],
+                            },{
+                                'message' : question_data[r_index+1],   
+                            },{
+                                'message' : question_data[r_index+2],
                             }
-                        }
+                        ]
                     else:
                         not_understood = "maaf, saya tidak paham yang anda maksud, bisa anda ulangi?"
-                        return not_understood
+                        payload = not_understood
+                # jika similiarity lebih dari 0.8
                 else:
+                    question_data = self.dataset['MESSAGE']
                     index_s.append(str(sims[i][0]))
                     score_s.append(str(sims[i][1]))
+                    # jawaban terbaik            
                     reply_indexes = pd.DataFrame({'index': index_s,'score': score_s})
                 
                 # find top question and answer
                 r_index = int(reply_indexes['index'].loc[0])
                 r_score = float(reply_indexes['score'].loc[0])
+                # jawaban terbaik
+                question = question_data[r_index]
                 reply = str(self.dataset.iloc[:,1][r_index])
 
-                return reply
-            
+                payload = {
+                    'question'  : question,
+                    'reply'     : reply,
+                }
+
+        return {
+            'status' : 200,
+            'result' : {
+                'payload' : payload
+            }
+        }
+    
     def lsa(self, sentence):
         reply = []
         reply = self.talk(str(sentence))        
