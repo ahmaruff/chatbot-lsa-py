4c37118472e4f88d6eb734f4fd09eda936b677f3
Ahmad Maruf <ahmadmaruf2701@gmail.com>
2023-07-20 01:13:23 +0700

    change dataset from csv to sqlite

--- app.py
+++ app.py
@@ -10,10 +10,11 @@ import os
 # from CHATBOT_LSA import lsa
 from LsaChatbot import LsaChatbot
 
+# SQLITE DATABASE
+DATABASE = 'database_lsa.db'
 stopword_path = os.getcwd() + '/stopword.txt'
-dataset_path = os.getcwd() + '/dataset/dataset.csv'
 
-lsaChatbot = LsaChatbot(stopword_path, dataset_path)
+lsaChatbot = LsaChatbot(stopword_path=stopword_path, db_path=DATABASE)
 
 def get_lsa_response(input_text):
     lsa_response = lsaChatbot.lsa(input_text)
@@ -22,10 +23,6 @@ def get_lsa_response(input_text):
     file_path = os.getcwd() + '/fallback_sentences.txt'
     with open(file_path, 'a') as file:
         file.write(input_text + "\n")
-
-
-# SQLITE DATABASE
-DATABASE = 'database_lsa.db'
 
 def get_db():
     db = getattr(g, '_database', None)
